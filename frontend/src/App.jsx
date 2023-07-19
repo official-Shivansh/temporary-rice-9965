a001c46aa87aef2c2cf3004a072065d194a217a4
Hemant Chaudhari <hemantchaudhari1587@gmail.com>
2023-07-19 22:38:24 +0530

    day1fw22_0346

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
+import CartPage from "./components/Cart/cart"
+import AllRoutes from "./routes/AllRoutes"
+
 function App() {
   return (
     <>
       <h1>ArtHub</h1>
+      <AllRoutes/>
 
     </>
   )
---
+++ frontend/src/routes/AllRoutes.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import CartPage from "../components/Cart/cart";
+
+const AllRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/cart" element={<CartPage/>}/>
+    </Routes>
+  )
+};
+
+export default AllRoutes;
