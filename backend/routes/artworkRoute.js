fa7a4cc6d55ca8ba5286a193d09599d67e90e3c6
chandrakalaM08 <chandrakala.2508@gmail.com>
2023-07-21 15:34:42 +0530

    Working on Post

--- backend/routes/artworkRoute.js
+++ backend/routes/artworkRoute.js
@@ -2,18 +2,10 @@ const { Router } = require("express");
 const { ArtworkModel } = require("../models/artworkModel");
 const artworkRouter = Router();
 
+
 artworkRouter.post("/addart", async (req, res) => {
   try {
-    let {
-      title,
-      image,
-      price,
-      tags,
-      description,
-      comments,
-      likes,
-      typeOfArtWork,
-    } = req.body;
+    let { title, price, tags, description, typeOfArtWork, image } = req.body;
     let newArt = await ArtworkModel.create({
       title,
       image,
@@ -23,10 +15,10 @@ artworkRouter.post("/addart", async (req, res) => {
       description,
       creator: req.userId,
       creator_name: req.username,
-      likes,
-      comments,
+      likes: [],
+      comments: [],
     });
-     await newArt.populate("creator");
+    await newArt.populate("creator");
     res.status(200).send({ msg: "Artwork added", art: newArt });
   } catch (error) {
     res.status(400).send({ error: error.message });
