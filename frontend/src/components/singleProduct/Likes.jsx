bf4916440c0ba4d713fddc84d6b806f4dfd48aea
chandrakalaM08 <chandrakala.2508@gmail.com>
2023-07-23 15:48:55 +0530

    Mapped data in home page and linked home page items to single product page

--- frontend/src/components/singleProduct/Likes.jsx
+++ frontend/src/components/singleProduct/Likes.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Box, Flex, IconButton, Badge } from "@chakra-ui/react";
 import { FaHeart } from "react-icons/fa";
-
+import { useParams } from "react-router-dom";
+import { handleLike } from "../../redux/reducers/artworkReducer/artworkAction";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 const imageData = {
   imageUrl: "https://example.com/artwork.jpg",
   comments: [
@@ -13,6 +16,25 @@ const imageData = {
 };
 
 export default function Likes() {
+
+  const { id } = useParams();
+  const userId = JSON.parse(localStorage.getItem("user"))
+
+  const [product, setProduct] = useState({});
+  const products = useSelector((store) => store.artworkReducer.allarts.arts);
+
+  // console.log(products, "products")
+  useEffect(() => {
+    const item = products?.find((element) => element._id === id);
+    setProduct(item)
+    console.log("item is", item)
+  }, [id]);
+
+  function clickingLike() {
+
+    console.log("inside clickingLike", userId._id)
+    handleLike(id, userId)
+  }
   return (
     <Box>
       <Flex mt="2" align="center" justify="space-between">
@@ -22,8 +44,9 @@ export default function Likes() {
           // size="sm"
           variant="ghost"
           color="gray.600"
+          onClick={clickingLike}
         />
-        <Badge colorScheme="pink">{imageData.likes} Likes</Badge>
+        <Badge colorScheme="pink">{product?.likes?.length} Likes</Badge>
       </Flex>
     </Box>
   );
